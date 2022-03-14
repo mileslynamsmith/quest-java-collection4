079bc87ce201d072b44469e289a1d5708a96bf85
Miles Lynam-Smith <miles.lynam-smith@telekom.de>
2022-03-14 16:32:39 +0100

    Collections Quest 4

--- Thanos.java
+++ Thanos.java
@@ -1,4 +1,7 @@
-public class Thanos {
+import java.util.*;
+import java.util.concurrent.*;
+
+public class Thanos  {
 
     public static void main(String[] args) {
 
@@ -17,14 +20,28 @@ public class Thanos {
         Flower rose = new Flower("Rose");
 
         // TODO 1 : Create a TreeMap where :
-        // Hulk, Thor and Scarlet Witch have a Rose
-        // Vision has a Tulip
-        // Captain America has a Lily
-        // Doctor Strange and Black Widow have a Violet
+        Comparator<Hero> comparator = new Comparator<Hero>() {  
+           @Override  
+            public int compare(Hero orig, Hero compare) {  
+                return orig.getName().compareTo(compare.getName());  
+           }  
+        }; 
+        TreeMap<Hero, Flower> party = new TreeMap<>(comparator);
+
+        party.put(hulk, rose); 
+        party.put(thor, rose);  
+        party.put(scarletWitch, rose);  
+        party.put(vision, tulip);
+        party.put(captainAmerica, lily);
+        party.put(doctorStrange, violet);
+        party.put(blackWidow, violet);
 
         // TODO 2 : Print if `begonia` is contained in the TreeMap
-
+        System.out.println(party.containsValue(begonia));
         // TODO 3 : For each hero, alphabetically, print the corresponding flower
-
+        for (Hero hero : party.keySet()) {
+            Flower flower = party.get(hero);
+            System.out.println(flower.getName());
+        }
     }
 }
